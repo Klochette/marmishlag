069f5891cb177ed18efc6fafb2012eded8b9b605
Klochette <melina@arkhn.com>
2022-03-09 16:23:55 +0100

    add login page

---
+++ wp-content/themes/wphetic/functions.php
@@ -0,0 +1,11 @@
+<?php
+add_action("after_setup_theme", "wphetic_theme_support");
+
+function wphetic_theme_support () {
+    add_theme_support('title-tag');
+}
+
+function wpdocs_theme_name_scripts() {
+    wp_enqueue_style( 'style-name', get_stylesheet_uri() ); /* enqueues style.css */
+}
+add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
